856af952f532cb3af14a66748e634eccc880ebd3
Nilkumar Patel <=>
2021-02-08 10:46:35 -0500

    Exercises 1-8 - Exercise 10.3 complete

--- Exercises 1-8/js/lab10-walkthrough03.js
+++ Exercises 1-8/js/lab10-walkthrough03.js
@@ -1,2 +1,9 @@
 /* try the different advanced selection calls here */
+// setting bg color to yellow for type password
+$(':password').css('background-color', 'yellow');
 
+// change color of every fourth child
+$('form	:nth-child(4n)').css('background-color', 'yellow');
+
+// select all labels with "word" inside and turn black
+$('label:contains("word")').css("color", "black");
