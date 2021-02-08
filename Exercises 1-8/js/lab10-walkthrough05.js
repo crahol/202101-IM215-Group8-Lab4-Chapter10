5eb437a83557612093fa07861f92294ea0e42f88
Nilkumar Patel <=>
2021-02-08 11:06:07 -0500

    Exercises 1-8 - Exercise 10.5 complete

--- Exercises 1-8/js/lab10-walkthrough05.js
+++ Exercises 1-8/js/lab10-walkthrough05.js
@@ -1,2 +1,22 @@
 /* put your DOM code here */
 
+// $(function() {
+//     //create a new DOM element
+//     var	img = $('<img src="images/art/thumbs/13030.jpg">');
+//     //and now add the new element after the	panel
+//     var	panel = $('.panel');
+//     panel.after(img);
+// });
+
+// now using other methods on panel and img instead of after
+$(function() {
+    //create a new DOM element
+    var	img = $('<img src="images/art/thumbs/13030.jpg">');
+    //and now add the new element after the	panel
+    var	panel = $('.panel');
+    // panel.before(img);
+    // panel.append(img);
+    // img.appendTo(panel);
+    // img.prependTo(panel);
+    img.insertBefore(panel);
+});
