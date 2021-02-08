6130003a6b5ab0ee4f61782be1bfe34d77c60bb4
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-02-08 10:57:44 -0500

    Exercise 1-8 - Exercise 10.4 done

--- Exercises 1-8/js/lab10-walkthrough04.js
+++ Exercises 1-8/js/lab10-walkthrough04.js
@@ -1,2 +1,35 @@
 /* put your event code here */
 
+// $(".panel").click(function()	{
+//     $("#message").html("You	clicked	in	the	panel");
+// });
+
+
+// $(".panel").on("click",	function()	{
+//     $("#message").html("You	clicked	in	the	panel");
+// });
+
+
+// $(function	()	{
+//     $(".panel").on("click",	function()	{
+//         $("#message").html("You	clicked	in	the	panel");
+//     });
+// });
+
+
+//it will display x and y value if moving mouse to the panel 
+// it will say good bye! if leave box
+// it will say stopped move reporting if user clicks on panel 
+$(function	()	{
+    $(".panel")
+        .on("mousemove",function (e) {
+            $("#message").html("x="	+ e.pageX +	" y=" +	e.pageY);
+        })
+        .on("mouseleave",function (e) {
+            $("#message").html("goodbye!");
+        })
+        .on("click",function() {
+            $("#message").html("stopped	move reporting");
+            $(".panel").off("mousemove");
+        });		
+});	
