9c9e20bca558d594aa643cca8b2afbab3004b373
Nilkumar Patel <=>
2021-02-22 11:23:12 -0500

    Exercise 9 completed - Nilkumar

---
+++ Exercises 9-10/Nilkumar Patel/js/lab10-walkthrough09.js
@@ -0,0 +1,19 @@
+$(function () {
+    $("#consume").click(function () {
+        var url = "http://www.randyconnolly.com/funwebdev/services/travel/cities.php";
+        $.get(url, function (data, status) {
+            var list = "";
+            $.each(data, function (index, value) {
+                var li = $('<li/>').html(value.name);
+                li.appendTo("div#results ul");
+            });
+            if (status == "success") {
+                var list = "";
+                for (var i = 0; i < data.length; i++) {
+                    list += data[i].name + "<br>";
+                }
+                $("#results").html(list);
+            }
+        });
+    });
+});
