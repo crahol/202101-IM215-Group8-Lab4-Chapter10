4eb043dccf0918d41b7a7b43bb197819e89fbc69
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-02-26 13:05:23 -0500

    Last 3 properties for sliders are done

--- project1/js/Project1.js
+++ project1/js/Project1.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var form = $('#sliderBox form');
     $('#thumbBox').click(function (e) {
         if (e.target.nodeName == 'IMG') {
-            
+
             var image = e.target;
             var imgSrc = image.src;
             var text = image.alt;
@@ -10,12 +10,12 @@ $(document).ready(function () {
 
             $('#imgManipulated img')[0].src = imgSrc.replace('small', 'medium');
             $('#imgManipulated figcaption')[0].innerHTML = '<em>' + text + '</em><br>' + title;
-            
+
         }
     });
 
-    
-    form.on('change', function(e) {
+
+    form.on('change', function (e) {
         var prop = e.target;
         var img = $('#imgManipulated img');
         switch (prop.id.toLowerCase()) {
@@ -32,6 +32,21 @@ $(document).ready(function () {
                 img.css('filter', 'brightness(' + prop.value + '%)');
                 img.css('-webkit-filter', 'brightness(' + prop.value + '%)');
                 $('#numBrightness').html(prop.value);
+                break;
+            case 'sliderhue':
+                img.css('filter', 'hue-rotate(' + prop.value + 'deg)');
+                img.css('-webkit-filter', 'hue-rotate(' + prop.value + 'deg)');
+                $('#numHue').html(prop.value);
+                break;
+            case 'slidergray':
+                img.css('filter', 'grayscale(' + prop.value + '%)');
+                img.css('-webkit-filter', 'grayscale(' + prop.value + '%)');
+                $('#numGray').html(prop.value);
+                break;
+            case 'sliderblur':
+                img.css('filter', 'blur(' + prop.value + 'px)');
+                img.css('-webkit-filter', 'blur(' + prop.value + 'px)');
+                $('#numBlur').html(prop.value);
                 break;
         }
     });
