167c492804a9a78f23f1a04b7a0155b330ec1f11
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-02-26 22:32:34 -0500

    Project2: done with mouseenter and displayed image with caption

--- project2/js/project2.js
+++ project2/js/project2.js
@@ -9,17 +9,41 @@ $(document).ready(function () {
     }
 
     $(function () {
-        $('ul.gallery img').on('mouseover', function(e) {
+        $('ul.gallery img').on('mouseover', function (e) {
+            var alt = $(this).attr('alt');
 
+            var text;
+            for (var i = 0; i < images.length; i++) {
+                if (alt == images[i].description) {
+                    var title = images[i].title;
+                    var city = images[i].city;
+                    var country = images[i].country;
+                    var date = images[i].taken;
+
+                    text = title + '<br>' + city + ', ' + country + ' [' + date + ']';
+
+                }
+            }
+            var src = $(this).attr('src');
+            var newsrc = src.replace("square", "medium");
+            var preview = $('<div id="preview"></div>');
+            var image = $('<img	src="' + newsrc + '">');
+            var caption = $('<p>' + text + '</p>');
+            preview.append(image);
+            preview.append(caption);
+            $('body').append(preview);
+            $(this).addClass("gray");
+            $("#preview").show();
         });
-
-        $("ul.gallery img").on("mouseleave", deletePreview);
-        $("ul.gallery img").on("mousemove", shiftPreview);
     });
 
-    function deletePreview(e) {
-    }
+    $("ul.gallery img").on("mouseleave", deletePreview);
+    $("ul.gallery img").on("mousemove", shiftPreview);
+});
 
-    function shiftPreview(e) {
-    }
+function deletePreview(e) {
+}
+
+function shiftPreview(e) {
+}
 });
